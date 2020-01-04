ca042c8e48465eec433d86f6ba339e68c800075a
Muhammad Moaz Khan <moazkh60@gmail.com>
2020-01-04 22:04:29 +0500

    menu-item style changed, hover effect added.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -41,8 +41,8 @@ const DirectoryMenu = () => {
 	  
 	return (
 		<div className="directory-menu">
-			{sections.map(({title, imageUrl, id}) => 
-				<MenuItem key={id} title={title} />
+			{sections.map(({title, imageUrl, id, size}) => 
+				<MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
 			)}
 		</div>
 	  );

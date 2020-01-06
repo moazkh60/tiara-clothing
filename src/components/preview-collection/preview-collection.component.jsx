574cef4dd62edcc99d4f992f15fa28844017796c
Muhammad Moaz Khan <moazkh60@gmail.com>
2020-01-06 13:01:37 +0500

    Updated shop page with collection item component. Added styles to collection-item, preview component

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './preview-collection.styles.scss';
-import { findAllByTitle } from '@testing-library/react';
+import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({title, items}) => {
 	return (
 		<div className='collection-preview'>
 			<h1 className='title'>{title.toUpperCase()}</h1>
 			<div className='preview'>
-				{items.filter((items,index) => index < 4).map(item => <div key={item.id}>{item.name}</div>)}
+				{items.filter((item,index) => index < 4).map(({id, ...otherItemProps}) => 
+					<CollectionItem key={id} {...otherItemProps} />)}
 			</div>
 		</div>
 	);

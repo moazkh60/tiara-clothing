ab3d4ce44ffe33c69802b4c2d5d7f35e491a2121
Muhammad Moaz Khan <moazkh60@gmail.com>
2020-01-08 23:33:16 +0500

    Create user profile document

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component'; 
 import Header from './components/header/header.component';
 import SignInSignUpPage from './pages/signInSignUp/signInSignUp.component';
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null)
 
   useEffect(() => {
-    let unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      setCurrentUser(user)
+    let unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user)
     })
     return () => { unsubscribeFromAuth() }
   }, [])

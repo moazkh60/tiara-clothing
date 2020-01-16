746443e74a76bd96f1d6871a06c16383bddc632e
Moaz Khan <moazkh60@gmail.com>
2020-01-16 11:24:22 +0500

    Updated cart utils to group items. Updated shop data.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
-import './collection-item.styles.scss';
-import CustomButton from '../custom-button/custom-button.component';
+import React from "react";
+import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
+import { useDispatch } from "react-redux";
+import { addItem } from "../../reducer/cart/cart.actions";
 
-const CollectionItem = ({id, name, price, imageUrl}) => {
-	return ( 
-		<div className='collection-item'>
-			<div className="image"
-				style={{backgroundImage: `url(${imageUrl})`}}
-			></div>
-			<div className='collection-footer'>
-				<span className='name'>{name}</span>
-				<span className='price'>{price}</span>
-			</div>
-			<CustomButton inverted>Add to Cart</CustomButton>
-		</div>
-	 );
-}
- 
+const CollectionItem = ({ item }) => {
+  const { name, price, imageUrl } = item;
+
+  const dispatch = useDispatch();
+
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton inverted onClick={() => dispatch(addItem(item))}>
+        Add to Cart
+      </CustomButton>
+    </div>
+  );
+};
+
 export default CollectionItem;

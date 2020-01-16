437152c3080bf177b5e171cca9cefd7e4ce989df
Moaz Khan <moazkh60@gmail.com>
2020-01-16 13:18:54 +0500

    Cart layout setup complete.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
+import { useSelector } from "react-redux";
+import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
+  const cartItems = useSelector(state => state.cart.cartItems);
+
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        <CustomButton> GO TO CHECKOUT</CustomButton>
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
       </div>
+      <CustomButton> GO TO CHECKOUT</CustomButton>
     </div>
   );
 };

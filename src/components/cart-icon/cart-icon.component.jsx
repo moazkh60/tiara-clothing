9d30bf94d414b7c087511a4a9b0c28c1f73855d5
Moaz Khan <moazkh60@gmail.com>
2020-01-16 17:57:07 +0500

    Set quantity in cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import "./cart-icon.styles.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/icons/11.2 shopping-bag.svg";
 import { toggleCartHidden } from "../../reducer/cart/cart.actions";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const CartIcon = () => {
 
-  const dispatch = useDispatch()
+  const quantity = useSelector(state =>
+    state.cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem + quantity,
+      0
+    )
+  );
+  const dispatch = useDispatch();
 
   return (
     <div className="cart-icon" onClick={() => dispatch(toggleCartHidden())}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{quantity}</span>
     </div>
   );
 };

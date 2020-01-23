13b22211fe446784ab7a634eb83ef8d89013b37a
Muhammad Moaz Khan <moazkh60@gmail.com>
2020-01-23 19:57:57 +0500

    Added add remove item in checkout item

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import './checkout-item.styles.scss';
+import React from "react";
+import "./checkout-item.styles.scss";
+import { useDispatch } from "react-redux";
+import { clearItemFromCart, addItem, removeItem } from "../../reducer/cart/cart.actions";
 
-const CheckoutItem = ({cartItem: {name, imageUrl, price, quantity}}) => {
-	return (<div className='checkout-item'>
-		<div className='image-container'>
-			<img alt='item' src={imageUrl}/>
-		</div>
-		<span className='name'>{name}</span>
-		<span className='quantity'>{quantity}</span>
-		<span className='price'>{price}</span>
-		<div className='remove-button'>&#10005;</div>
-	</div>
-	);
-}
- 
+const CheckoutItem = ({ cartItem }) => {
+
+  const  { name, imageUrl, price, quantity } = cartItem
+  const dispatch = useDispatch()
+  
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img alt="item" src={imageUrl} />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+		  <div className="arrow" onClick={() => dispatch(removeItem(cartItem))}>&#10094;</div>
+		  <span className='value'>{quantity}</span>
+		  <div className="arrow" onClick={() => dispatch(addItem(cartItem))}>&#10095;</div>
+		  </span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => dispatch(clearItemFromCart(cartItem))}>&#10005;</div>
+    </div>
+  );
+};
+
 export default CheckoutItem;

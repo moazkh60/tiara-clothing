5f10991397202908bb0ad1b272cbb94ec2325003
Muhammad Moaz Khan <moazkh60@gmail.com>
2020-01-24 19:57:31 +0500

    Shop and Directory code refactoring

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import SHOP_DATA from './shop.data';
 import CollectionPreview from '../../components/preview-collection/preview-collection.component.jsx';
+import { useSelector } from 'react-redux';
+import { selectCollections } from '../../reducer/shop/shop.selector';
 
 const ShopPage = () => {
-	const [collections, setCollections] = useState(SHOP_DATA)
+	const collections = useSelector(state =>
+		selectCollections(state)
+	)
 	return (  
 		<div className="shop-page">
 			{collections.map(({id, ...otherCollectionProps}) => 
